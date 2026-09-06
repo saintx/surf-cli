5061cada810d03b8c1791019ff032ee21196f369
Alexander Saint Croix <alex@saintx.us>
2026-09-06 01:35:30 -0400

    Bump version to 0.4.3

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       in {
         packages.default = pkgs.python312Packages.buildPythonApplication {
           pname = "surf";
-          version = "0.4.2";
+          version = "0.4.3";
           src = ./.;
           pyproject = true;
 
